41db7c9e2af3a1f725f80254a0cbf90bed7484de
Tanner Mills <tanner.mills2000@gmail.com>
2023-11-03 13:13:23 -0600

    Feature: Added setCoordinates method and validR method. Updated Satellite constructor to use new validR method. Also added a throws clause in the constructor.

--- src/Bodies/Satellite.java
+++ src/Bodies/Satellite.java
@@ -3,13 +3,25 @@ package Bodies;
 public class Satellite extends Body{
     private final IllegalArgumentException badRadius = new IllegalArgumentException(
             "Distance number invalid. Distance must be greater than " + Earth.getRadius());
-    public Satellite(double r, double theta, double z) {
-        if (r < Earth.getRadius()) {
+    public Satellite(double r, double theta, double z) throws IllegalArgumentException{
+        if (!validR(r)) {
             throw badRadius;
         } else {
             this.r = r;
         }
         this.theta = theta;
         this.z = z;
+    }
+    public void setCoordinates(double r, double theta, double z) throws IllegalArgumentException{
+        if (!validR(r)) {
+            throw badRadius;
+        } else {
+            setR(r);
+        }
+        setTheta(theta);
+        setZ(z);
+    }
+    private boolean validR(double r)  {
+        return r > Earth.getRadius();
     }
 }
